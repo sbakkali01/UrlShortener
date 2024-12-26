a1d145ce16f62bb80c63013efa189dce7bc2a753
sbakkali01 <bakkalisami01@gmail.com>
2024-12-26 19:45:32 +0100

    created usecase if link already exists

--- main.py
+++ main.py
@@ -33,17 +33,24 @@ def fetch_from_db(query):
     return data
 
 @app.post('/submit',response_class=HTMLResponse)
-def get_long_url(data=Form()):
-    short_url=data
-    short_url=short_url.split("://")[-1]
-    hash=hashlib.sha1(short_url.encode("UTF-8")).hexdigest()
-    short_url="http://127.0.0.1:8000"+"/redirect/"+hash[:10]
-    insert_into_db({"long_url":data, "short_url":short_url})
-    return "<div> All good ! </div>"
+def get_long_url(request:Request, data=Form()):
+    res=fetch_from_db(f"""select * from my_table where long_url='{data}' """)
+
+    if res:
+        short_url=res[0][1]
+    else:
+        short_url = res
+        short_url = short_url.split("://")[-1]
+        hash = hashlib.sha1(short_url.encode("UTF-8")).hexdigest()
+        short_url="http://127.0.0.1:8000"+"/redirect/"+hash[:10]
+        insert_into_db({"long_url":res, "short_url":short_url})
+
+    return templates.TemplateResponse(request=request, name="after.html", context={"short_url":short_url,"long_url":data})
+
 
 @app.get('/redirect/{token}',response_class=RedirectResponse)
 def redirect_to_long_url(token):
     short_url="http://127.0.0.1:8000"+"/redirect/"+token
-    data=fetch_from_db(f"""select * from my_table where short_url='{short_url}' """ )
+    data=fetch_from_db(f"""select * from my_table where short_url='{short_url}' """)
     long_url=data[0][0]
     return RedirectResponse(long_url)
